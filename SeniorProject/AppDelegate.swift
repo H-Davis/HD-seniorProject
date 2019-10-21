e564af2bfac6036babbca09ea913bd280a469f1b
Hananiah Davis <hananiahd97@gmail.com>
2019-10-21 12:34:16 -0400

    Initial Commit

---
+++ SeniorProject/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SeniorProject
+//
+//  Created by Hananiah Davis on 10/21/19.
+//  Copyright © 2019 HananiahDavis. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SeniorProject/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SeniorProject
+//
+//  Created by Hananiah Davis on 10/21/19.
+//  Copyright © 2019 HananiahDavis. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
