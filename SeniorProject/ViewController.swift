f872116cd71bb3d66a12fa31f5957cce03fe6dbf
Hananiah Davis <hananiahd97@gmail.com>
2019-11-07 15:40:42 -0500

    - Now able to get a photo based on a keyword - I can put that image into the UnsplashImages folder - Updated the layout of UI Window

--- SeniorProject/ViewController.swift
+++ SeniorProject/ViewController.swift
@@ -25,11 +25,14 @@ class ViewController: NSViewController {
         }
     }
     
+    struct myVariables {
+        //global variables
+    }
     
 
     @IBAction func createFolder(_ sender: Any) {
         let downloadsDirectory = FileManager.default.urls(for: .downloadsDirectory, in: .userDomainMask).first!
-        let downloadsDirectoryWithFolder = downloadsDirectory.appendingPathComponent("Unsplash Images")
+        let downloadsDirectoryWithFolder = downloadsDirectory.appendingPathComponent("UnsplashImages")
         
         do {
             try FileManager.default.createDirectory(at: downloadsDirectoryWithFolder, withIntermediateDirectories: true, attributes: nil)
@@ -38,5 +41,45 @@ class ViewController: NSViewController {
         }
     }
     
+    
+    @IBAction func saveImage(_ sender: Any) {
+        let downloadsUrl:URL =  FileManager.default.homeDirectoryForCurrentUser
+        //urls(for: .downloadsDirectory, in: .userDomainMask).first!
+        let imagePath = "Downloads/UnsplashImages"
+        let destinationFileUrl = downloadsUrl.appendingPathComponent(imagePath).appendingPathComponent("newImage.jpg")
+        
+        //Create URL to the source file you want to download
+        let fileURL = URL(string: "https://source.unsplash.com/featured/?,city")
+        
+        let sessionConfig = URLSessionConfiguration.default
+        let session = URLSession(configuration: sessionConfig)
+        
+        let request = URLRequest(url:fileURL!)
+        
+        let task = session.downloadTask(with: request) { (tempLocalUrl, response, error) in
+            if let tempLocalUrl = tempLocalUrl, error == nil {
+                // Success
+                if let statusCode = (response as? HTTPURLResponse)?.statusCode {
+                    print("Successfully downloaded. Status code: \(statusCode)")
+                }
+                
+                do {
+                    try FileManager.default.copyItem(at: tempLocalUrl, to: destinationFileUrl)
+                } catch (let writeError) {
+                    print("Error creating a file \(destinationFileUrl) : \(writeError)")
+                }
+                
+            } else {
+                print("Error took place while downloading a file. Error description: %@", error!.localizedDescription);
+            }
+            
+        }
+        task.resume()
+        
+    }
 }
+
+
+    
+
 
